915be83a3229ac42b5b53a9c654c0064e1c8f160
mamund <mamund@yahoo.com>
2012-06-06 02:12:48 +0000

    update

--- get-todo-filter.js
+++ get-todo-filter.js
@@ -9,5 +9,7 @@ request({
   headers: {"Accept": "text/html", "Authorization": "Basic xxxxxxxxxxxxxxxxxxxx"}, 
   method: "GET"
 }, function (error, response, body) {
+  console.log("Status", response.statusCode);
+  console.log("Headers", JSON.stringify(response.headers));
   console.log("Reponse received", body);
 });
